425364177d55d8a70a2e17119553c0bd5088ddf4
Jeffrey Liu <jeffffrey@gmail.com>
2015-06-21 01:55:05 +0000

    update latest

---
+++ start2.js
@@ -0,0 +1,36 @@
+var sensorName   = 'humix-sense-nfc';
+var natChannel   = 'humix.sense.nfc.command';
+var workDir      = '/home/liuch/workspace/humix/humix-sense/controls/humix-sense-nfc';
+var pythonScript = workDir + '/detectNFC.py';
+var processing   = false;
+var isClosed     = true;
+
+var net  = require('net');
+var fs   = require('fs');
+var ps   = require('child_process');
+var nats = require('nats').connect();//connect immediately
+
+//unlink domainSocket anyway
+
+//the following server is only used for shutdown service
+//once someone connects to the domain socket and close
+//this service will shutdown
+var server = net.createServer(function(conn) { 
+    conn.on('end', function () {
+        nats.close();
+        server.unref();
+    });
+});
+
+server.on('error', function (err) {
+    console.error('failed to start service,' + err);
+    nats.close();
+    server.unref();
+});
+
+nats.subscribe('humix.sense.nfc.status.ping', function(request,replyto){
+    console.log(' [NFC] received ping')
+    console.log('sending pong');
+    nats.publish(replyto, 'humix.sense.nfc.status.pong');
+    
+});
